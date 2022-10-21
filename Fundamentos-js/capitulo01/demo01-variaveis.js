bf8d9dce1260a46a9befd3adac4a8bd5a43461d9
Lucas Ulerich <lucas.ulerich@to-brasil.com>
2022-10-21 11:48:14 -0300

    Aulas do capitulo um do curso FUNDAMENTOS_JS

---
+++ Fundamentos-js/capitulo01/demo01-variaveis.js
@@ -0,0 +1,22 @@
+// let é o iniciador, sem ela não podemos iniciar a
+// a variavel
+
+// seguido declaramos o nome da variavel
+// Depois declaramos o valor que a variavel vai receber, variaveis podem ser criadas sem valor.
+
+let nome = "Super man"
+let idade = 100
+let poder = "Super força"
+
+nome = "Batman"
+idade = 32
+poder = "Rico"
+
+console.log(
+  `Nome: ${nome}
+   Idade: ${idade}
+   Poder: ${poder}`
+)
+
+//No javascript, existem palavras revervadas que não podem ser usadas como variaves, pois elas já são próprias do javascript.
+
---
+++ Fundamentos-js/capitulo01/demo03-operadores.js
@@ -0,0 +1,14 @@
+console.log('1 + 1 é:', 1+1) // Operador de adição
+console.log('2 - 4 é:', 2-4) // Operador de subtração
+console.log('3 * 6 é:', 3*6) // Operador de multiplicação
+console.log('5 / 20 é:', 5/20) // Operador de divisão
+console.log('2 % 4 é:', 2%4) // Operador de resto
+console.log('2 ** 4 é', 2 **4) // Operador de exponenciação
+
+console.log('------------Operadores ---------------\n')
+console.log('1 > 2 é:', 1>2)
+console.log('1 < 2 é:', 1<2)
+console.log('1 >= 2 é:', 1>2)
+console.log('1 >= 2 é:', 1===2)
+console.log('1 === 2 é:', 1>2)
+console.log('1 !== 2 é:', 1!==2)
